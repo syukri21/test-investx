93744fdbc3dccb7be7a3d23ba5568365e0188aa5
syukri <syukrihsb148@gmail.com>
2020-08-21 02:04:43 +0700

    add packagejson

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,7 +3,13 @@ import React from "react";
 interface Props {}
 
 const App = (props: Props) => {
-    return <div></div>;
+    return (
+        <input
+            className='bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal'
+            type='email'
+            placeholder='jane@example.com'
+        ></input>
+    );
 };
 
 export default App;
