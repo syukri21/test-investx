792d9b33c8e3d8ee2f52847df2ff5c2fdeb14364
syukri <syukrihsb148@gmail.com>
2020-08-21 04:50:40 +0700

    fix button

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -15,13 +15,13 @@ const Header = (props: Props) => {
                     <span className='text-3xl font-medium'>Photos</span>
                 </div>
                 <div className='grid grid-cols-2 divide-x divide-gray-600'>
-                    <div className="flex items-center mr-4 justify-center">
+                    <div className='flex items-center mr-4 justify-center'>
                         <Button>
                             <FontAwesomeIcon className='text-gray-600' icon={faCloudUploadAlt} />
                             <span className='text-gray-600 ml-2'>Upload</span>
                         </Button>
                     </div>
-                    <div className="flex items-center justify-start pl-4" >
+                    <div className='flex items-center justify-start pl-4'>
                         <Button>
                             <span className='text-gray-600 mr-2'>25</span>
                             <FontAwesomeIcon className='text-gray-600' icon={faChevronDown} />
