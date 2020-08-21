26648eb6fa4c0a1a39d3839f42e8988d5f0fc680
syukri <syukrihsb148@gmail.com>
2020-08-21 07:17:00 +0700

    fix refresh

--- src/hooks/use.photo-list.ts
+++ src/hooks/use.photo-list.ts
@@ -4,7 +4,11 @@ import PhotoRepository from "../repositories/PhotoRepository";
 
 export default function usePhotoList({ limit = 25, skip = 0 }) {
     const params = useMemo(() => ["/photos/list", { skip: 0, limit }], [limit]);
-    const { data, error, mutate } = useSWR<PhotoType.PhotoListResponses>(params, PhotoRepository.getAllPhoto, { refreshInterval: 0 });
+    const { data, error, mutate } = useSWR<PhotoType.PhotoListResponses>(params, PhotoRepository.getAllPhoto, {
+        refreshInterval: 0,
+        revalidateOnFocus: false,
+        refreshWhenHidden: false,
+    });
 
     const loading = !data && !error;
 
