ea35b709b9fbf956229d2e7c126bbf4d51fae60c
syukri <syukrihsb148@gmail.com>
2020-08-21 07:14:14 +0700

    fix add lastpage

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,41 +6,18 @@ import useApp from "./hooks/useApp";
 
 import { withAppProvider } from "./context/AppContext";
 import Button from "../components/atoms/Button";
-import useAppContext from "./hooks/useAppContext";
-import PhotoRepository from "../repositories/PhotoRepository";
 
 interface Props {}
 
 const App = (props: Props) => {
-    const { pagination, dropdown } = useAppContext();
-    const { photoList } = useApp();
-
-    const handleLoadMore = React.useCallback(async () => {
-        try {
-            const newData: PhotoType.PhotoListResponses = await PhotoRepository.getAllPhoto("/photos/list", {
-                skip: (pagination.state.page + 1) * dropdown.state.data,
-                limit: dropdown.state.data,
-            });
-
-            pagination.handleNextPage(newData.documents.length < dropdown.state.data);
-            photoList.mutate(
-                (data: any) => ({
-                    ...data,
-                    documents: [...data.documents, ...newData.documents],
-                }),
-                false
-            );
-        } catch (e) {
-            console.log(e.message);
-        }
-    }, [pagination, dropdown.state.data, photoList]);
+    const { photoList, handleLoadMore, isLastPage } = useApp();
 
     return (
         <React.Fragment>
             <Header></Header>
             <PhotoList {...photoList}></PhotoList>
             <div className='flex justify-center items-center py-8'>
-                {!pagination.state.isLastPage && (
+                {!isLastPage && (
                     <Button width='auto' onClick={handleLoadMore}>
                         <span className='text-gray-600'>Load More</span>
                     </Button>
--- src/pages/hooks/useApp.tsx
+++ src/pages/hooks/useApp.tsx
@@ -1,6 +1,7 @@
 import usePhotoList from "../../hooks/use.photo-list";
 import useAppContext from "./useAppContext";
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
+import PhotoRepository from "../../repositories/PhotoRepository";
 
 export default function useApp() {
     const { dropdown, pagination } = useAppContext();
@@ -13,5 +14,25 @@ export default function useApp() {
         // eslint-disable-next-line
     }, [dropdown.state.data]);
 
-    return { photoList };
+    const handleLoadMore = useCallback(async () => {
+        try {
+            const newData: PhotoType.PhotoListResponses = await PhotoRepository.getAllPhoto("/photos/list", {
+                skip: (pagination.state.page + 1) * dropdown.state.data,
+                limit: dropdown.state.data,
+            });
+
+            pagination.handleNextPage(newData.documents.length < dropdown.state.data);
+            photoList.mutate(
+                (data: any) => ({
+                    ...data,
+                    documents: [...data.documents, ...newData.documents],
+                }),
+                false
+            );
+        } catch (e) {
+            console.log(e.message);
+        }
+    }, [pagination, dropdown.state.data, photoList]);
+
+    return { photoList, handleLoadMore, isLastPage: pagination.state.isLastPage };
 }
